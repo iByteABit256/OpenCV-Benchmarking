e22ca22c9e4d805585a4cfef7c7aad405a36378d
pauls <pauls@Ubuntu-Focal-Fossa.myguest.virtualbox.org>
2023-01-26 12:24:33 -0800

    Finalised benchmark automation & notebook

--- benchmark.py
+++ benchmark.py
@@ -2,29 +2,37 @@ import sys
 import pathlib
 import subprocess
 import csv
+from datetime import datetime
 
 opencv_root_dir = '/home/pauls/opencv/4.7.0/'
-csv_file = 'benchmark-results.csv'
+
+def generate_filename():
+    return 'benchmark-results/benchmark-results_' + datetime.now().strftime('%m-%d-%Y_%H:%M:%S') + '.csv'
 
 def output_result_to_csv(results):
-    header = ['Build', 'Average execution time (μs)']
+    header = ['Build', 'Execution time (μs)']
 
-    with open(csv_file, 'w') as f:
+    csv_file = generate_filename()
+    with open(csv_file, 'w+') as f:
         writer = csv.DictWriter(f, fieldnames=header)
         writer.writeheader()
         writer.writerows(results)
+    print(f'Saved benchmark results in {csv_file}')
 
-def get_avg_execution_micros(out):
+def get_execution_micros(out):
     res = out.split('\n')[-2]
-    avg_exec_time = res.split('[')[1].split('μs')[0]
-    return avg_exec_time
+    avg_exec_time = res.split('[')[1].split('μs')[0].strip()
+    return int(avg_exec_time)
 
-def get_opencv_builds():
-    opencv_dir = pathlib.Path(opencv_root_dir)
-    opencv_builds = [str(item).split('/')[-1] for item in opencv_dir.iterdir() if str(item) != opencv_root_dir + 'opencv-4.7.0']
-    print(f'Found {len(opencv_builds)} builds: {opencv_builds}\n')
-    
-    return opencv_builds
+def benchmark(build, num_of_executions):
+    benchmark_results = []
+    for i in range(num_of_executions):
+        out = subprocess.run(['./benchmark.sh', '1'], check=True, capture_output=True)
+        execution_micros = get_execution_micros(out.stdout.decode('utf-8'))
+        benchmark_results.append({'Build': build, 'Execution time (μs)': execution_micros})
+        print(f'Build {build} took {execution_micros} μs for execution #{i}')
+    print("")
+    return benchmark_results
 
 def update_cmake(build):
     cmake_config = 'CMakeLists.txt'
@@ -42,6 +50,13 @@ def update_cmake(build):
             line = line.replace(curr_build, new_build)
             f.write(line)
 
+def get_opencv_builds():
+    opencv_dir = pathlib.Path(opencv_root_dir)
+    opencv_builds = [str(item).split('/')[-1] for item in opencv_dir.iterdir() if str(item) != opencv_root_dir + 'opencv-4.7.0']
+    print(f'Found {len(opencv_builds)} builds: {opencv_builds}\n')
+    
+    return opencv_builds
+
 """
 
  For every openCV build, install it, update the CMakeLists.txt file,
@@ -50,19 +65,15 @@ def update_cmake(build):
 """
 def main():
     opencv_builds = get_opencv_builds()
-    num_of_executions = 15 if len(sys.argv) == 1 else sys.argv[1]
+    num_of_executions = 15 if len(sys.argv) == 1 else int(sys.argv[1])
     benchmark_results = []
 
     for build in opencv_builds:
         subprocess.run(['./install-build.sh', opencv_root_dir + build], check=True)
         update_cmake(build)
-        out = subprocess.run(['./benchmark.sh', num_of_executions], check=True, capture_output=True)
-        avg_execution_micros = get_avg_execution_micros(out.stdout.decode('utf-8'))
-        benchmark_results.append({'Build': build, 'Average execution time (μs)': avg_execution_micros})
-        print(f'Build {build} took an average of {avg_execution_micros} μs for {num_of_executions} executions\n')
+        benchmark_results = benchmark_results + benchmark(build, num_of_executions)
 
     output_result_to_csv(benchmark_results)
-    print(f'Saved benchmark results in {csv_file}')
 
 
 if __name__ == '__main__':
